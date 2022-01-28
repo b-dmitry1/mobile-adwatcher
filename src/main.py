a15aa824f2aa67fb11a36501bdd8bac0eb55d696
Sergey Bogachev <sergeyvbo@outlook.com>
2022-01-29 01:14:37 +0300

    2

--- src/main.py
+++ src/main.py
@@ -164,11 +164,21 @@ while(True):
     screenshot = wincap.get_screenshot()
     screenshot = np.array(screenshot)
 
+
+
     rectangles = []
     files = [filename for filename in os.listdir('.\\img\\')]
     for f in files:
         rects = getrekt('img\\'+ f, screenshot)
+        for (x, y, w, h) in rects:
+            cv2.rectangle(screenshot, (x, y), (x + w, y + h), getcolor(f), 2)
         rectangles.extend(rects)
 
-    for (x, y, w, h, filename) in rectangles:
-        cv2.rectangle(screenshot, (x, y), (x + w, y + h), getcolor(filename), 2)
+    cv2.imshow('Screen', screenshot)
+
+    if len(rectangles) > 0:            
+        (x, y, w, h) = random.choice(rectangles)
+        mouse.position = wincap.get_screen_position((x + w / 2, y + h / 2))
+        mouse.click(Button.left, 1)
+        if cv2.waitKey(35000) == ord('c'):
+            continue
